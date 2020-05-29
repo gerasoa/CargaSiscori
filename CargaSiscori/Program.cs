747b538823234d28b4232c91001d7952c4e65530
Rogerio Alves Soares <gerasoa@gmail.comgit config --global user.name Rogerio>
2020-05-29 18:06:55 +0100

    Add project files.

---
+++ CargaSiscori/Program.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CargaSiscori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportarCSV();
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+    
+        private static void ImportarCSV()
+        {
+            var arquivos = ObterArquivos();
+        }
+
+        private static IList<string> ObterArquivos()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
