0327d18a93892d2bfed152ec664e8c127ecd9d90
Rogerio Alves Soares <gerasoa@gmail.comgit config --global user.name Rogerio>
2020-05-29 21:04:48 +0100

    Criacao do mecanismo de leitura do csv

--- CargaSiscori/Program.cs
+++ CargaSiscori/Program.cs
@@ -3,29 +3,129 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using CargaSiscori.Models;
+using CsvHelper.Configuration;
+using System.Globalization;
+using CargaSiscori.ViewModels;
 
 namespace CargaSiscori
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
-            ImportarCSV();
+            ProcessarArquivos();
 
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
 
-
-    
-        private static void ImportarCSV()
+        private static bool ProcessarArquivos()
         {
-            var arquivos = ObterArquivos();
-        }
+            var status = true;
 
-        private static IList<string> ObterArquivos()
+            if (!VerificaPastasParaImportacao())
+                return false;
+
+            var arquivosParaImportacao = ListarArquivosParaImportacao();
+
+            if (arquivosParaImportacao == null)
+                status = false;
+
+            foreach (var arquivo in arquivosParaImportacao)
+            {
+                var importacoesDoCapituloMensal = LerArquivoCSV(arquivo);
+                Adicionar(importacoesDoCapituloMensal);
+            }
+
+            return status;
+        }        
+        
+        private static IList<Siscori> LerArquivoCSV(string arquivo)
         {
-            throw new NotImplementedException();
+            List<Siscori> importacoesDoCapitulo = new List<Siscori>();
+
+            using (var reader = new StreamReader(arquivo, Encoding.GetEncoding("iso-8859-1")))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.Delimiter = "@";
+                csv.Configuration.BadDataFound = null;
+                csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.Trim().ToUpper();
+                csv.Configuration.IgnoreQuotes = true;
+
+                var records = csv.GetRecords<SiscoriViewModel>();
+
+                if (records == null)
+                    return null;
+
+                foreach (var record in records)
+                {
+                    importacoesDoCapitulo.Add(new Siscori
+                    {
+                        AnoMes = record.AnoMes.Trim(),
+                        DescricaoDoProduto = record.DescricaoDoProduto.Trim(), //.Length > 8000 ? record.DescricaoDoProduto.Substring(1, 7999).ToString() : record.DescricaoDoProduto,
+                        Incoterm = record.Incoterm.Trim(),
+                        NatInformacao = record.NatInformacao.Trim(),
+                        NcmCodigo = record.NcmCodigo.Trim(),
+                        NcmDescricao = record.NcmDescricao.Trim(),
+                        Ordem = record.Ordem.Trim(),
+                        PaisAquisicaoCodigo = record.PaisAquisicaoCodigo.Trim(),
+                        PaisDeAquisicao = record.PaisDeAquisicao.Trim(),
+                        PaisDeOrigem = record.PaisDeOrigem.Trim(),
+                        PaisOrigemCodigo = record.PaisOrigemCodigo.Trim(),
+                        PesoLiquido = record.PesoLiquido.Trim(),
+                        QuantidadeComercial = record.QuantidadeComercial.Trim(),
+                        QuantidadeEstatistica = record.QuantidadeEstatistica.Trim(),
+                        SituacaoDoDespacho = record.SituacaoDoDespacho.Trim(),
+                        TotalunidadeProdutoDolar = record.TotalunidadeProdutoDolar.Trim(),
+                        UnidadeComercial = record.UnidadeComercial.Trim(),
+                        UnidadeDeMedida = record.UnidadeDeMedida.Trim(),
+                        UnidadeDesembaraco = record.UnidadeDesembaraco.Trim(),
+                        UnidadeDesembarque = record.UnidadeDesembarque.Trim(),
+                        UnidEstat = record.UnidEstat.Trim(),
+                        ValorSeguroDolar = record.ValorSeguroDolar.Trim(),
+                        ValorUnidadeProdutoDolar = record.ValorUnidadeProdutoDolar.Trim(),
+                        VolumeDolar = record.VolumeDolar.Trim(),
+                        VoumeFrenteDolar = record.VoumeFrenteDolar.Trim()
+                    });
+                }
+            }
+            return importacoesDoCapitulo;
         }
 
+        private static string[] ListarArquivosParaImportacao()
+        {
+            if (VerificaPastasParaImportacao())
+            {
+                return Directory.GetFiles(@"C:\Temp\Siscori\Novos", "*.csv");
+            }
+
+            return null;
+        }
+
+        private static bool VerificaPastasParaImportacao()
+        {
+            var status = true;
+
+            var novosArquivos = @"C:\Temp\Siscori\Novos";
+            var processados = @"C:\Temp\Siscori\Processados";
+
+            if (!Directory.Exists(processados))
+                Directory.CreateDirectory(processados);
+
+            if (!Directory.Exists(novosArquivos))
+            {
+                Directory.CreateDirectory(novosArquivos);
+                Console.WriteLine(string.Format("Foi criada a pasta para importação dos arquivos .csv em: {0}.", novosArquivos));
+                status = false;
+            }
+
+            return status;
+        }
+
+        private static void Adicionar(IList<Siscori> entity)
+        {
+            //DB.BulkInsert(entity);
+        }
     }
 }
---
+++ CargaSiscori/ViewModels/SiscoriViewModel.cs
@@ -0,0 +1,85 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CargaSiscori.ViewModels
+{
+    public class SiscoriViewModel
+    {
+        [Name("NUMERO DE ORDEM")]
+        public string Ordem { get; set; }
+
+        [Name("ANOMES")]
+        public string AnoMes { get; set; }
+
+        [Name("COD.NCM")]
+        public string NcmCodigo { get; set; }
+
+        [Name("DESCRICAO DO CODIGO NCM")]
+        public string NcmDescricao { get; set; }
+
+        [Name("PAIS")]
+        public string PaisOrigemCodigo { get; set; }
+
+        [Name("PAIS DE ORIGEM")]
+        public string PaisDeOrigem { get; set; }
+
+        [Name("PAIS")]
+        public string PaisAquisicaoCodigo { get; set; }
+
+        [Name("PAIS DE AQUISICAO")]
+        public string PaisDeAquisicao { get; set; }
+
+        [Name("UND.ESTAT.")]
+        public string UnidEstat { get; set; }
+
+        [Name("UNIDADE DE MEDIDA")]
+        public string UnidadeDeMedida { get; set; }
+
+        [Name("UNIDADE COMERC.")]
+        public string UnidadeComercial { get; set; }
+
+        [Name("DESCRICAO DO PRODUTO")]
+        public string DescricaoDoProduto { get; set; }
+
+        [Name("QTDE ESTATISTICA")]
+        public string QuantidadeEstatistica { get; set; }
+
+        [Name("PESO LIQUIDO")]
+        public string PesoLiquido { get; set; }
+
+        [Name("VMLE DOLAR")]
+        public string VolumeDolar { get; set; }
+
+        [Name("VL FRETE DOLAR")]
+        public string VoumeFrenteDolar { get; set; }
+
+        [Name("VL SEGURO DOLAR")]
+        public string ValorSeguroDolar { get; set; }
+
+        [Name("VALOR UN.PROD.DOLAR")]
+        public string ValorUnidadeProdutoDolar { get; set; }
+
+        [Name("QTD COMERCIAL.")]
+        public string QuantidadeComercial { get; set; }
+
+        [Name("TOT.UN.PROD.DOLAR")]
+        public string TotalunidadeProdutoDolar { get; set; }
+
+        [Name("UNIDADE DESEMBARQUE")]
+        public string UnidadeDesembarque { get; set; }
+
+        [Name("UNIDADE DESEMBARACO")]
+        public string UnidadeDesembaraco { get; set; }
+
+        [Name("INCOTERM")]
+        public string Incoterm { get; set; }
+
+        [Name("NAT.INFORMACAO")]
+        public string NatInformacao { get; set; }
+
+        [Name("SITUACAO DO DESPACHO")]
+        public string SituacaoDoDespacho { get; set; }
+    }
+}
