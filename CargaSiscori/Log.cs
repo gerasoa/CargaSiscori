3585935f6dbf1251c3101d84be5bb915d1bd5a83
Rogerio Alves Soares <gerasoa@gmail.comgit config --global user.name Rogerio>
2020-05-30 21:40:44 +0100

    Implementacao do sistema de log

---
+++ CargaSiscori/Log.cs
@@ -0,0 +1,27 @@
+﻿using CargaSiscori.Context;
+using CargaSiscori.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CargaSiscori
+{
+    public class Log
+    {
+        public static void LogGravacao(LogEntity log)
+        {
+            using (var db = new BWContext())
+            {
+                try
+                {
+                    db.Add(log);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message); 
+                }                
+            }
+        }        
+    }
+}
